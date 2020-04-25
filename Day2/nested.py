cfc817587077b81cc7197fbf2c6e8878b5f25e40
akannshu <akanshuraj@gmail.com>
2020-04-25 16:48:06 +0530

    added day3 and Day4

--- Day2/nested.py
+++ Day2/nested.py
@@ -1,6 +1,6 @@
 n = int(input("Enter a range for numbers: "))
 for i in range(2,n+1):
-    for j in range(1,11):
+    for j in range(10,0,-1):
         print(i*j,end=" ")
 
     print("")
---
+++ Day4/str.py
@@ -0,0 +1,19 @@
+# s = "string"
+# d1 = 'string2'                    
+
+"""
+string2string  --> Tanmoyee, Rithic, Harshita, Bhargavi, Joshna
+error --> Swarup 
+string2 --> Swarup 
+
+"""
+
+
+#d1 += s   #concatenation
+# print(d1)
+# ls = ["Tanmoyee ", "Rithic ", "Harshita ", "Bhargavi ", "Joshna"]
+# s = ""
+# for x in ls:
+#     s+=x
+
+# print(s)    
