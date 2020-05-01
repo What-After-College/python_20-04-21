af11b1c23d31c208885089847d21f9f49b1a9716
akannshu <akanshuraj@gmail.com>
2020-05-01 17:43:31 +0530

    added html and css files required for day 10

--- Day9/app.py
+++ Day9/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # @app.route("/ak")
@@ -13,9 +13,18 @@ app = Flask(__name__)
 # def superman():
 #     return "The man of steel"
 
-@app.route("/<name>")
-def hello(name):
-    return "Hello " + name
+# @app.route("/<name>")
+# def hello(name):
+#     return "<h1> Hello </h1>" + name
+
+@app.route("/home")
+def homePage():
+    return render_template("home.html")
+
+@app.route("/")
+def htmlPage():
+    return render_template("index.html")
+
 
 
 if __name__ == "__main__":
