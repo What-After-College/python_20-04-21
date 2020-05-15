88ffe3281d6e7c234fff267b879f0651bc68bc1f
Akanshu <akanshuraj@gmail.com>
2020-05-15 14:24:54 +0530

    Update fol.py

--- Day06/fol.py
+++ Day06/fol.py
@@ -5,7 +5,7 @@ print(df.head())
 import folium
 from folium import plugins
 
-stationArr = df[['X','Y']].values
+stationArr = df[['Y','X']].values
 
 m = folium.Map(location=[stationArr[0][0],stationArr[0][1]], zoom_start=15)
 m.add_child(plugins.HeatMap(stationArr[:50000], radius=15))
